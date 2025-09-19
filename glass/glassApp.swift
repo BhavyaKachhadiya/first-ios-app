1b7e48b35876a722979018329f8e198fcd6563e3
BhavyaKachhadiya <kachhadiyabhavya@gmail.com>
2025-09-19 20:59:06 +0530

    Initial Commit

---
+++ glass/glassApp.swift
@@ -0,0 +1,17 @@
+//
+//  glassApp.swift
+//  glass
+//
+//  Created by Bhavya Kachhadiya on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct glassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

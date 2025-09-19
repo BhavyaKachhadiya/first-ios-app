93e54f5c182a0c57b9b69aaf0ea7495024061209
BhavyaKachhadiya <kachhadiyabhavya@gmail.com>
2025-09-19 23:43:49 +0530

    create a first ios app!!

---
+++ glass/componet/LoanOfferCard.swift
@@ -0,0 +1,18 @@
+//
+//  LoanOfferCard.swift
+//  glass
+//
+//  Created by Bhavya Kachhadiya on 9/19/25.
+//
+
+import SwiftUI
+
+struct LoanOfferCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoanOfferCard()
+}
